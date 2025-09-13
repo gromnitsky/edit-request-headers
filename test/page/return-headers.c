b7b40efb13456e1e8cbfb43eec762b78bec9808f
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2025-09-13 22:27:32 +0300

    return-headers test: use fgets

--- test/page/return-headers.c
+++ test/page/return-headers.c
@@ -1,3 +1,4 @@
+#define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,53 +18,50 @@ struct Header* header_init() {
   return hdr;
 }
 
-int header_valid(char *s, ssize_t len) {
+int header_line_valid(char *s, int len) {
   if (len < 2) return 0;
   return s[len-2] == '\r' && s[len-1] == '\n';
 }
 
-char* substring(char* src, char c) {
-  if (!src) return NULL;
-  char* end = strchr(src, c); if (!end) return NULL;
-  size_t len = end - src;
+char* substring(char* src, char *c) {
+  if ( !(src && c)) return NULL;
+  int len = c - src;
   char* dest = malloc(len + 1); assert(dest);
   memcpy(dest, src, len);
   dest[len] = '\0';
   return dest;
 }
 
+#define HEADER_LINE_MAX 16*1024
+#define HEADERS_SIZE 32*1024
+
 struct Header* headers() {
-  char *header = NULL;
-  size_t dummy = 0;
-  ssize_t header_len;
-  int total_headers_size = 0;
-  int lines = 0;
+  char header[HEADER_LINE_MAX+1];
+  int header_len, lines = 0, total_headers_size = 0;
   struct Header *hdr, *last = NULL;
 
-  while ( (header_len = getline(&header, &dummy, stdin)) != -1) {
-    if (!header_valid(header, header_len)) return NULL;
+  while (fgets(header, HEADER_LINE_MAX+1, stdin)) {
+    header_len = strlen(header);
+    if (!header_line_valid(header, header_len)) return NULL;
 
     ++lines;
     if (lines == 1) continue;   /* skip HTTP method line */
     if (header_len == 2) break; /* end of headers */
 
-    fprintf(stderr, "line %d (%c)\n", lines, header[0]);
-
     total_headers_size += header_len;
-    if (total_headers_size > 32*1024) return NULL;
+    if (total_headers_size > HEADERS_SIZE) return NULL;
 
     header[header_len-2] = '\0';
     hdr = header_init();
 
-    hdr->name = substring(header, ':'); if (NULL == hdr->name) return NULL;
-    char *colon = strchr(header, ':');
+    char *colon = strchr(header, ':'); if (NULL == colon) return NULL;
+    hdr->name = substring(header, colon);
     if (header_len - (colon - header) > 4) hdr->value = strdup(colon + 2);
 
     hdr->next = last;
     last = hdr;
   }
 
-  free(header);
   return last;
 }
 
